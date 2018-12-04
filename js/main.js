8b6c11c285738d9b094fe4d235bd95fdab889e5f
nikthecreator <nikifor.shapran@gmail.com>
2018-12-04 19:20:34 +0300

    commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function externalLinks() {
+    links = document.getElementsByTagName("a");
+    for (i=0; i<links.length; i++) {
+      link = links[i];
+      if (link.getAttribute("href") && link.getAttribute("rel") == "external")
+      link.target = "_blank";
+    }
+   }
+   window.onload = externalLinks;
