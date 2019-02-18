dcc0b060637303b7c5e537a47bf1aef3195895ca
nikthecreator <nikifor.shapran@gmail.com>
2019-02-18 18:35:21 +0300

    final commit

--- js/main.js
+++ js/main.js
@@ -11,19 +11,11 @@ function externalLinks() {
 
 /* Open when someone clicks on the span element */
 function openNav() {
-  document.getElementById("myNav").style.width = "100%";
+  document.getElementById("myNavDesk", "myNav").style.width = "100%";
 }
 
 /* Close when someone clicks on the "x" symbol inside the overlay */
 function closeNav() {
-  document.getElementById("myNav").style.width = "0%";
+  document.getElementById("myNavDesk", "myNav").style.width = "0%";
 }
 
-function openNav() {
-  document.getElementById("myNavDesk").style.width = "100%";
-}
-
-/* Close when someone clicks on the "x" symbol inside the overlay */
-function closeNav() {
-  document.getElementById("myNavDesk").style.width = "0%";
-}
