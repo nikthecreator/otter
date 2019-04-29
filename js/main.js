25d7bb28d1b424a04137c295d9a4429f973349d1
nikthecreator <nikifor.shapran@gmail.com>
2019-04-29 13:14:32 +0300

    slick slider done

--- js/main.js
+++ js/main.js
@@ -19,3 +19,40 @@ function closeNav() {
   document.getElementById("myNavDesk", "myNav").style.width = "0%";
 }
 
+//slick slider
+
+// Slick Slider
+$('.slide').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  centerMode: true,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
